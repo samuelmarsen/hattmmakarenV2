b2e2a705d8f675e430e31d822d64d0ae8491db0c
Petterson2020 <simonpetterson20@gmail.com>
2026-04-17 11:59:57 +0200

    Tillbaka

--- hattmakaren2026_9/src/hattmakaren2026_9/MaterialBestallningsSidan.java
+++ hattmakaren2026_9/src/hattmakaren2026_9/MaterialBestallningsSidan.java
@@ -64,6 +64,7 @@ this.idb = idb;
         jScrollPane3 = new javax.swing.JScrollPane();
         txtLogg = new javax.swing.JTextArea();
         jLabel5 = new javax.swing.JLabel();
+        btnTillbakaKnapp = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -118,6 +119,9 @@ this.idb = idb;
 
         jLabel5.setText("Format:             YYYY-MM-DD");
 
+        btnTillbakaKnapp.setText("Tillbaka");
+        btnTillbakaKnapp.addActionListener(this::btnTillbakaKnappActionPerformed);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -149,7 +153,10 @@ this.idb = idb;
                         .addComponent(btnSokDatum, javax.swing.GroupLayout.PREFERRED_SIZE, 64, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
                         .addContainerGap()
-                        .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 175, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 175, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(btnTillbakaKnapp)))
                 .addContainerGap(683, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -180,7 +187,9 @@ this.idb = idb;
                     .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 392, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(btnSkapaPdf)
-                .addContainerGap(517, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 473, Short.MAX_VALUE)
+                .addComponent(btnTillbakaKnapp)
+                .addGap(21, 21, 21))
         );
 
         pack();
@@ -413,9 +422,13 @@ uppdateraTabell();
         // TODO add your handling code here:
     }//GEN-LAST:event_txtFranDatumActionPerformed
 
+    private void btnTillbakaKnappActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnTillbakaKnappActionPerformed
+this.dispose();    }//GEN-LAST:event_btnTillbakaKnappActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnSkapaPdf;
     private javax.swing.JButton btnSokDatum;
+    private javax.swing.JButton btnTillbakaKnapp;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
