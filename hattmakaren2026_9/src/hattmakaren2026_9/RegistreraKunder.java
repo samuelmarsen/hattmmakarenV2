63ec22b5e54023079e4b942780a0a5a9bfc6a70d
klucasbrautigam-lab <klucas.brautigam@gmail.com>
2026-04-21 11:37:44 +0200

    gränssnitt registrera kunder

--- hattmakaren2026_9/src/hattmakaren2026_9/RegistreraKunder.java
+++ hattmakaren2026_9/src/hattmakaren2026_9/RegistreraKunder.java
@@ -28,6 +28,7 @@ public class RegistreraKunder extends javax.swing.JFrame {
     public RegistreraKunder(InfDB idb, Kundsida kundsida) {
         this.idb = idb;
         this.kundsida = kundsida;
+        this.setExtendedState(javax.swing.JFrame.MAXIMIZED_BOTH);
   
         initComponents();
 
@@ -43,18 +44,18 @@ public class RegistreraKunder extends javax.swing.JFrame {
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
-        java.awt.GridBagConstraints gridBagConstraints;
 
-        txtNamn = new javax.swing.JTextField();
-        txtMejladress = new javax.swing.JTextField();
-        txtAdress = new javax.swing.JTextField();
-        txtTelefonnummer = new javax.swing.JTextField();
-        jLabel1 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
         jLabel3 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        btnRegNyKund = new javax.swing.JButton();
+        txtTelefonnummer = new javax.swing.JTextField();
         BTNtillbaka = new javax.swing.JButton();
+        txtNamn = new javax.swing.JTextField();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        txtMejladress = new javax.swing.JTextField();
+        btnRegNyKund = new javax.swing.JButton();
+        txtAdress = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setMaximumSize(new java.awt.Dimension(650, 450));
@@ -62,63 +63,53 @@ public class RegistreraKunder extends javax.swing.JFrame {
         setPreferredSize(new java.awt.Dimension(650, 450));
         setSize(new java.awt.Dimension(650, 450));
 
-        txtNamn.addActionListener(this::txtNamnActionPerformed);
+        jPanel1.setMaximumSize(new java.awt.Dimension(500, 400));
+        jPanel1.setMinimumSize(new java.awt.Dimension(500, 400));
 
-        txtAdress.addActionListener(this::txtAdressActionPerformed);
+        jLabel3.setText("Epost");
 
         txtTelefonnummer.addActionListener(this::txtTelefonnummerActionPerformed);
 
-        jLabel1.setText("Telefon");
+        BTNtillbaka.setText("Tillbaka");
+        BTNtillbaka.addActionListener(this::BTNtillbakaActionPerformed);
+
+        txtNamn.addActionListener(this::txtNamnActionPerformed);
 
         jLabel4.setText("Adress");
 
-        jLabel3.setText("Epost");
+        jLabel1.setText("Telefon");
 
         jLabel2.setText("Namn");
 
         btnRegNyKund.setText("Registrera ny kund");
         btnRegNyKund.addActionListener(this::btnRegNyKundActionPerformed);
 
-        BTNtillbaka.setText("Tillbaka");
-        BTNtillbaka.addActionListener(this::BTNtillbakaActionPerformed);
+        txtAdress.addActionListener(this::txtAdressActionPerformed);
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(btnRegNyKund))
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(BTNtillbaka))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addContainerGap()
-                                .addComponent(txtNamn, javax.swing.GroupLayout.PREFERRED_SIZE, 497, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
-                                .addGap(16, 16, 16)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jLabel2)
-                                    .addComponent(jLabel1)
-                                    .addComponent(jLabel3)
-                                    .addComponent(jLabel4)
-                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                                        .addComponent(txtTelefonnummer, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 497, Short.MAX_VALUE)
-                                        .addComponent(txtMejladress, javax.swing.GroupLayout.Alignment.LEADING)
-                                        .addComponent(txtAdress, javax.swing.GroupLayout.Alignment.LEADING)))))
-                        .addGap(0, 17, Short.MAX_VALUE)))
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(txtNamn, javax.swing.GroupLayout.PREFERRED_SIZE, 497, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel4, javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                        .addComponent(txtTelefonnummer, javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(txtMejladress, javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(txtAdress, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 497, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(btnRegNyKund, javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(BTNtillbaka, javax.swing.GroupLayout.Alignment.LEADING))
                 .addContainerGap())
         );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(BTNtillbaka)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jLabel2)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(txtNamn, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -134,9 +125,28 @@ public class RegistreraKunder extends javax.swing.JFrame {
                 .addComponent(jLabel4)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(txtAdress, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 24, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(btnRegNyKund)
+                .addGap(18, 18, 18)
+                .addComponent(BTNtillbaka)
                 .addContainerGap())
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(16, 16, 16)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(135, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(35, 35, 35)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
@@ -204,6 +214,7 @@ public class RegistreraKunder extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JPanel jPanel1;
     private javax.swing.JTextField txtAdress;
     private javax.swing.JTextField txtMejladress;
     private javax.swing.JTextField txtNamn;
